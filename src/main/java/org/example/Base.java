dd444b393e2568e81360071915550b3829be19cd
Zara Martín <zmartand@myuax.es>
2023-05-04 19:40:45 +0200

    clase producto Base

--- src/main/java/org/example/Base.java
+++ src/main/java/org/example/Base.java
@@ -1,30 +1,39 @@
 package org.example;
-/*Ejercicio 1
-
-El Servicio de Emergencias Sanitarias de Madrid, antes conocido como 112, está organizado en una jerarquía de bases. Algunas de estas bases realmente disponen de personal y ambulancias (por ejemplo la de Villanueva de la Cañada), mientras que otras son «unidades administrativas» que agrupan un conjunto de bases (por ejemplo Majadahonda, que agrupa la unidad del Hospital Clínico Universitario Puerta de Hierro y la de Alcobendas entre otras).
-
-Para cada base, interesa modelar los siguientes datos:
-
-Nombre de la base.
-Número de ambulancias, que en el caso de las bases compuestas es la suma de las ambulancias disponibles en las bases que las componen en ese momento.
-Tiempo medio de asistencia, que en el caso de las bases compuestas es la media de los tiempos medios de asistencia registrados en las bases que las componen en ese momento.
-Elaborar un diseño que permita modelar adecuadamente esta situación, implentando completamente en Replit las clases que modelan las bases, sean del tipo que sean. Definir los patrones de diseño utilizado*/
+// Clase Producto
 public class Base {
-  private String nombre;
-    private int numAmbulancias;
-    private int tiempoMedioAsistencia;
+    private  String nombre;
+    private  int numAmbulancias;
+    private  double tiempoMedioAsistencia;
 
+    public Base(String nombre, int ambulancias, double tiempoMedioAsistencia) {
+        this.nombre = nombre;
+        this.numAmbulancias = ambulancias;
+        this.tiempoMedioAsistencia = tiempoMedioAsistencia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getAmbulancias() {
+        return numAmbulancias;
+    }
+
+    public double getTiempoMedioAsistencia() {
+        return tiempoMedioAsistencia;
+    }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    public void setNumAmbulancias(int numAmbulancias) {
-        this.numAmbulancias = numAmbulancias;
+    public void setAmbulancias(int ambulancias) {
+        this.numAmbulancias = ambulancias;
     }
-    public void setTiempoMedioAsistencia(int tiempoMedioAsistencia) {
+    public void setTiempoMedioAsistencia(double tiempoMedioAsistencia) {
         this.tiempoMedioAsistencia = tiempoMedioAsistencia;
     }
 
-
-
 }
+
+
+
