460f8d084c4479b328abdf97caf5362beadb9d16
Zara Martín <zmartand@myuax.es>
2023-05-04 19:41:19 +0200

    Abstract Builder (BaseBuilder)

--- src/main/java/org/example/BaseBuilder.java
+++ src/main/java/org/example/BaseBuilder.java
@@ -1,16 +1,8 @@
 package org.example;
 //Clase Abstract Builder
-public abstract class BaseBuilder {
-   protected  Base base;
-   public Base getBase(){
-       return base;
-   }
-    public void crearBase(){
-         base = new Base();
-    }
-public abstract BaseBuilder setNombre(String nombre);
-public abstract BaseBuilder setNumAmbulancias(int numAmbulancias);
-public abstract BaseBuilder setTiempoMedioAsistencia(int tiempoMedioAsistencia);
 
+public interface BaseBuilder {
+    void setNombre(String nombre);
+    void addAmbulancia(int numAmbulancias);
+    void addTiempoMedioAsistencia(double tiempoMedioAsistencia);
 }
-
